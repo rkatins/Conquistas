58047fc35524a8a609c9f4b0345aeac3ab40db54
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2023-07-09 20:00:39 +0200

    Iniciador

---
+++ vista/Iniciar.java
@@ -0,0 +1,13 @@
+package vista;
+
+import controlador.Comprobador;
+
+public class Iniciar {
+
+	public static void main(String[] args) {
+		Comprobador comprobador = new Comprobador();
+		
+		comprobador.mEmpezarComprobaciones();
+	}
+
+}
