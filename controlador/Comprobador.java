ef6ac9ad75b96661a8c9bafa09801c16e992e316
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2023-07-10 11:54:19 +0200

    Revisar Modelo -Fichero
    Muestra el id correcto pero aveces no encuentra el segundo fichero

--- controlador/Comprobador.java
+++ controlador/Comprobador.java
@@ -14,65 +14,81 @@ public class Comprobador {
 		
 		while (conquistados) {
 			int aleatorio = (int)(Math.random() * 185) + 1;
+			System.out.println("aleatorio -> " + aleatorio);
 			
-			int idFichero1 = fichero.mLeerPropiedadesPrimero(aleatorio);
-			int ejercitoFichero1 = 0;
+			int idFichero1 = 0;
+			int ejercitoFichero1;
+			idFichero1 = fichero.mLeerPropiedadesPrimero(aleatorio);
 			
-			mActualizarArrayList();
-			
-			int idFichero2 = 0;
-			for (int i = 0; i < paises.size(); i++) {
-				if (paises.get(i).getID() == idFichero1) {
-					ejercitoFichero1 = paises.get(i).getEjercito();
-					idFichero2 = paises.get(i).getPaisVecino().get((int)(Math.random() * paises.get(i).getPaisVecino().size()) + 1);
-					fichero.mLeerPropiedadesSegundo(idFichero2);
-					
-					mActualizarArrayList();
-				}
-			}
-			
-			int ejercitoFichero2 = 0;
-			for (int i = 0; i < paises.size(); i++) {
-				if (paises.get(i).getID() == idFichero2) {
-					ejercitoFichero2 = paises.get(i).getEjercito();
-				}
-			}
-			
-			if (ejercitoFichero1 > ejercitoFichero2) {
-				String nombreFichero1 = null;
-				String nombreFichero2 = null;
-				
-				for (int i = 0; i < paises.size(); i++) {
-					if (paises.get(i).getID() == idFichero1) {						
-						nombreFichero1 = paises.get(i).getNombre();
-					}
-					
-					if (paises.get(i).getID() == idFichero2) {
-						paises.get(i).setConquistado(true);
-						
-						nombreFichero2 = paises.get(i).getNombre();
-					}
-				}
-				
-				System.err.println("En la batalla entre " + nombreFichero1 + " y " + nombreFichero2 + ": GANA " + nombreFichero1);
-			} else if (ejercitoFichero1 < ejercitoFichero2) {
-				String nombreFichero1 = null;
-				String nombreFichero2 = null;
-					
-				for (int i = 0; i < paises.size(); i++) {
-					if (paises.get(i).getID() == idFichero1) {
-						nombreFichero1 = paises.get(i).getNombre();
-						
-						paises.get(i).setConquistado(true);
-					}
-					
-					if (paises.get(i).getID() == idFichero2) {
-						nombreFichero2 = paises.get(i).getNombre();
-					}
-				}
-				
-				System.err.println("En la batalla entre " + nombreFichero1 + " y " + nombreFichero2 + ": GANA " + nombreFichero1);
-			}
+			conquistados = false;
+//			mActualizarArrayList();
+//			
+//			int idFichero2 = 0;
+//			for (int i = 0; i < paises.size(); i++) {
+//				if (paises.get(i).getID() == idFichero1) {
+//					ejercitoFichero1 = paises.get(i).getEjercito();
+//					
+//					try {
+//						idFichero2 = paises.get(i).getPaisVecino().get((int)(Math.random() * paises.get(i).getPaisVecino().size()));
+//					} catch (NullPointerException e) {
+//					}
+//					
+//					try {
+//						for (int j = 0; j < paises.size(); j++) {
+//							System.out.println("posible idFichero2 -> " + paises.get(i).getPaisVecino().get(j));
+//						}
+//					} catch (NullPointerException e) {
+//					}
+//
+//					System.out.println("idFichero2 -> " + idFichero2);
+//					fichero.mLeerPropiedadesSegundo(idFichero2);
+//					
+//					mActualizarArrayList();
+//				}
+//			}
+//			
+//			int ejercitoFichero2 = 0;
+//			for (int i = 0; i < paises.size(); i++) {
+//				if (paises.get(i).getID() == idFichero2) {
+//					ejercitoFichero2 = paises.get(i).getEjercito();
+//				}
+//			}
+//			
+//			if (ejercitoFichero1 > ejercitoFichero2) {
+//				String nombreFichero1 = null;
+//				String nombreFichero2 = null;
+//				
+//				for (int i = 0; i < paises.size(); i++) {
+//					if (paises.get(i).getID() == idFichero1) {						
+//						nombreFichero1 = paises.get(i).getNombre();
+//					}
+//					
+//					if (paises.get(i).getID() == idFichero2) {
+//						paises.get(i).setConquistado(true);
+//						
+//						nombreFichero2 = paises.get(i).getNombre();
+//					}
+//				}
+//				
+//				System.err.println("En la batalla entre " + nombreFichero1 + " y " + nombreFichero2 + ": GANA " + nombreFichero1);
+//			} else if (ejercitoFichero1 < ejercitoFichero2) {
+//				String nombreFichero1 = null;
+//				String nombreFichero2 = null;
+//					
+//				for (int i = 0; i < paises.size(); i++) {
+//					if (paises.get(i).getID() == idFichero1) {
+//						nombreFichero1 = paises.get(i).getNombre();
+//						
+//						paises.get(i).setConquistado(true);
+//					}
+//					
+//					if (paises.get(i).getID() == idFichero2) {
+//						nombreFichero2 = paises.get(i).getNombre();
+//					}
+//				}
+//				
+//				System.err.println("En la batalla entre " + nombreFichero1 + " y " + nombreFichero2 + ": GANA " + nombreFichero1);
+//			}
 		}
 	}
 	
